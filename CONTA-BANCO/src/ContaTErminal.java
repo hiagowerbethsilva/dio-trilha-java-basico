6c85b51d3cd82f6ed536c39da59b7837b6a4f84a
hiago silva <hiagow16@gmail.com>
2025-03-23 20:00:45 -0300

    resolvendo problemas no scanner.nextDouble

--- CONTA-BANCO/src/ContaTErminal.java
+++ CONTA-BANCO/src/ContaTErminal.java
@@ -1,11 +1,45 @@
-public class ContaTErminal {
+import java.util.Scanner;
+
+
+public class ContaTerminal {
+
+      public static void main (String[] args) {
+//TODO: conhecer e importar a class scanner
+         Scanner scanner = new Scanner(System.in);
+         double saldo = 0;
+//exibir as mensagens para o usuario
+         System.out.println("digite o numero da conta !");
+         int numeroConta = scanner.nextInt();
+
+         System.out.println("digite o numero da Agência !");
+         String numeroAgencia  = scanner.next();
+
+         System.out.println("digite seu nome completo ! ");
+         String nome = scanner.next();
+
+         System.out.println("digite seu saldo !");
+
+        if (scanner.hasNextDouble()) {
+        saldo = scanner.nextDouble();
+
+        } else {
+            System.out.println("Entrada invalida ! por favor, insira numeros [.] e não [,] ");
+        saldo = scanner.nextDouble();
+
+        } 
+        
+
+
+//obter pelo scanner os valores digitados no terminal
+
+
+
+//exibir a mensagem conta criada
 
 
 
 
+    }
 
 
-
-
-    
 }
